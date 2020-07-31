7d8b4df144649e17cabdf903c3347fc46dba650a
Chan <33272151+lch8798@users.noreply.github.com>
2020-07-31 11:57:53 +0900

    add. CSS-in-JS

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,18 +1,36 @@
 import React from 'react';
 import Link from 'next/link';
 
+import styled from 'styled-components';
+
 export default function Nav(props) {
     return (
-        <nav>
-            <Link href={'/'}>
-                <a>Index</a>
-            </Link>
-            <Link href={'/test/1'}>
-                <a>Test1</a>
-            </Link>
-            <Link href={'/test/2'}>
-                <a>Test2</a>
-            </Link>
-        </nav>
+        <NavWrap>
+            <NavItem>
+                <Link href={'/'}>
+                    <a>Index</a>
+                </Link>
+            </NavItem>
+            <NavItem>
+                <Link href={'/test/1'}>
+                    <a>Test1</a>
+                </Link>
+            </NavItem>
+            <NavItem>
+                <Link href={'/test/2'}>
+                    <a>Test2</a>
+                </Link>
+            </NavItem>
+        </NavWrap>
     );
 }
+
+const NavWrap = styled.nav`
+    display: flex;
+    padding: 10px;
+    background: #eee;
+`;
+
+const NavItem = styled.div`
+    margin: 0 10px;
+`;
